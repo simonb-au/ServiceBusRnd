026fdeddb5fb3235fb7f07a2d6c993f6f8180431
Simon Bateman <simon.bateman@containerchain.com>
2014-06-23 14:16:05 +1000

    Added common versioning and build location

---
+++ CommonVersion.cs
@@ -0,0 +1,10 @@
+// Common Version information applicable to all assemblies
+using System;
+using System.Reflection;
+
+[assembly: AssemblyCompany("Eltesto")]
+[assembly: AssemblyProduct("MultiComs2")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
